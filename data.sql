09e33ab894bb8a6a162296bd7ab0e8a7bf16c8fe
emrearmand <rheyarmand47@gmail.com>
2025-03-22 17:40:19 +0700

    Add data.sql

---
+++ data.sql
@@ -0,0 +1,11 @@
+-- Insert data into customers table
+INSERT INTO customers (name, email) VALUES
+('David Raya', 'rayasaves22@example.com'),
+('William Saliba', 'willo2@example.com'),
+('Martin Odegaard', 'ode8@example.com');
+
+-- Insert data into products table
+INSERT INTO products (name, price) VALUES
+('Nike Shoes', 7000000),
+('Adidas Shoes', 9000000),
+('Puma Shoes', 500000);
