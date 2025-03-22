79b8df71e6060b5cec13c6f1d381ba85ea585875
emrearmand <rheyarmand47@gmail.com>
2025-03-22 17:55:38 +0700

    Add customers, products, orders

--- queries.sql
+++ queries.sql
@@ -10,3 +10,12 @@ CREATE TABLE products (
 INSERT INTO products (name, price) VALUES
 ('Product A', 10000),
 ('Product B', 20000);
+
+-- Menampilkan semua pelanggan
+SELECT * FROM customers;
+
+-- Menampilkan semua produk dengan harga di atas 1 juta
+SELECT * FROM products WHERE price > 1000000;
+
+-- Menampilkan pesanan terbaru
+SELECT * FROM orders ORDER BY order_date DESC;
